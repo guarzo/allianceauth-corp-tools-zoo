909584ddf92222b3ff3abb3a38dd1c06bff66787
Guarzo <guarzo.eve@gmail.com>
2025-11-20 01:19:53 -0500

    fix nonsense

--- corptools/__init__.py
+++ corptools/__init__.py
@@ -1,5 +1,5 @@
 """
 This module is the core of the CorpTools Ecosystem
 """
-__version__ = "2.15.1"
-__url__ = "https://github.com/Solar-Helix-Independent-Transport/allianceauth-corp-tools"
+__version__ = "0.0.6"
+__url__ = "https://github.com/guarzo/allianceauth-corp-tools-zoo"
